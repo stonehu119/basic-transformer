5aac64c691c302045ace3e9d0e7a258a780cc314
Stone Hu <stonehu9000@gmail.com>
2026-03-06 21:49:25 -0500

    rename to cuda version

--- start.py
+++ start.py
@@ -8,3 +8,5 @@ from torch.utils.data import Dataset, DataLoader, random_split
 from torchvision import transforms
 from torchmetrics import R2Score
 from os import sys
+import random
+import numpy as np
