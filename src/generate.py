0e9d27ea9d1d98cf41e8f5b674b80aa0f6fa7790
Stone Hu <stone.hu@venuiti.com>
2026-06-12 11:49:39 -0400

    some more error fixes

--- src/generate.py
+++ src/generate.py
@@ -1,28 +1,33 @@
 import argparse
+from typing import cast
 
 from miditok import REMI, MusicTokenizer, TokSequence
 from pathlib import Path
 from model import MidiGenerator, MidiLightningModule
 import torch
-import numpy as np
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 def generate_from_midi(
   model: MidiGenerator,
-  midi_path,
-  output_path = "out/test.midi",
-  tokenizer = REMI(params=Path("tokenizer.json")),
-  max_tokens = 1024,
+  midi_path: Path | str,
+  output_path: Path | str = "out/test.midi",
+  tokenizer: MusicTokenizer = REMI(params=Path("tokenizer.json")),
+  max_tokens: int = 1400,
 ):
   torch.set_default_device(device)
   model.eval()
   tokens = load_and_tokenize(midi_path, tokenizer)
-  generated_tokens = model.generate(input = tokens, max_len = max_tokens, eos_token_id=tokenizer["EOS_None"])
+  generated_tokens = model.generate(
+    input = tokens,
+    max_len = max_tokens,
+    eos_token_id=cast(int, tokenizer["EOS_None"]),
+    temperature=0.85
+  )
   save_tokens_to_file(generated_tokens, output_path, tokenizer)
 
-def load_and_tokenize(midi_path, tokenizer: MusicTokenizer):
-  tokenized_midi: list[TokSequence] = tokenizer.encode(midi_path)
+def load_and_tokenize(midi_path: Path | str, tokenizer: MusicTokenizer):
+  tokenized_midi: list[TokSequence] = tokenizer.encode(midi_path) # type: ignore
 
   # save tokenized sequence to file as a sanity check
   with open("out/token_stream.txt", "w") as f:
@@ -31,23 +36,23 @@ def load_and_tokenize(midi_path, tokenizer: MusicTokenizer):
   out = torch.tensor(tokenized_midi)
 
   # prepend BOS token to input sequence
-  bos_token = tokenizer["BOS_None"]
+  bos_token = cast(int, tokenizer["BOS_None"])
   prepend = torch.full((1, 1), bos_token)
   out = torch.cat([prepend, out], dim=1)
-  out = out[:, :1024]
+  out = out[:, :1600]
 
   print(out.shape)
   return out
 
-def save_tokens_to_file(token_stream: torch.Tensor, output_path, tokenizer: MusicTokenizer):
+def save_tokens_to_file(token_stream: torch.Tensor, output_path: Path | str, tokenizer: MusicTokenizer):
   print(token_stream.shape)
   token_stream = token_stream[:, 1:-1]
-  token_stream = token_stream.squeeze(0).cpu().tolist()
-  token_sequence = TokSequence(ids=token_stream, are_ids_encoded=True)
+  token_stream_list = cast(list[int | list[int]], token_stream.squeeze(0).cpu().tolist()) # type: ignore
+  token_sequence = TokSequence(ids=token_stream_list, are_ids_encoded=True)
   tokenizer.decode_token_ids(token_sequence)
   tokenizer.complete_sequence(token_sequence)
   print(token_sequence.tokens[:5])
-  tokenizer.decode([token_sequence], programs=[(0, False)], output_path=output_path)
+  tokenizer.decode([token_sequence], programs=[(0, False)], output_path=output_path) # type: ignore
 
 if __name__ == "__main__":
   torch.set_default_device(device)
@@ -57,19 +62,19 @@ if __name__ == "__main__":
   parser.add_argument(
     "--checkpoint-path",
     type=Path,
-    default="saved_checkpoints/midi-12-15964-3.125.ckpt",
+    default="saved_checkpoints/9_3k-context-loss=3.083.ckpt",
     help="Path to .ckpt file",
   )
   parser.add_argument(
     "--midi-path",
     type=Path,
-    default="test/arabesque.midi",
+    default="test/rachmaninoff.midi",
     help="Path to .midi file",
   )
   args = parser.parse_args()
 
   # Architecture is restored from the checkpoint's saved hyperparameters.
-  lightning_wrapper = MidiLightningModule.load_from_checkpoint(args.checkpoint_path)
+  lightning_wrapper = MidiLightningModule.load_from_checkpoint(args.checkpoint_path) # pyright: ignore[reportUnknownMemberType]
   model = lightning_wrapper.model
 
   generate_from_midi(model, midi_path = args.midi_path)
